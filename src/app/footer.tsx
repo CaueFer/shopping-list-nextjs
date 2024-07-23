00628d0ba40e04713d1b49e2c391610c3e634e5e
CaueFer <cauefernandescaetano@gmail.com>
2024-07-23 14:31:02 -0300

    add fonts

---
+++ src/app/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <div className="px-6 py-4 bg-white w-full flex  h-24">
+      <div className="flex-1 text-black">
+        <i className="bx bx-home-alt"></i>
+        Home
+      </div>
+    </div>
+  );
+}
